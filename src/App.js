c8f0ffe53f2021e488af6503fd516b1664f3d86a
Mihai Bundea <mihaibundea1@gmail.com>
2024-10-30 11:47:39 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import XRayViewer from './components/XRayViewer';
+
+function App() {
+  return (
+    <div className="App">
+      <XRayViewer />
+    </div>
+  );
+}
+
+export default App;
